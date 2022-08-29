7b3a4f62bf4c0d7d7c6dce43e1c28eff9c682e6c
Martina Peker <Martinaivanapeker@gmail.com>
2022-08-29 17:36:17 -0300

    NavBar

--- mi-tienda/src/App.js
+++ mi-tienda/src/App.js
@@ -1,13 +1,18 @@
 import productos from "./data/productos.json"
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+    <div class= "p-5">
+      <h2>Productos</h2>
         {
           productos.map(producto =>            
-            <li> Producto: {producto.nombre} ${producto.valor}<hr/></li>
+            <div class="card m-3 text-center"> Producto: {producto.nombre} ${producto.valor}</div>
             )
         }
+        </div>
     </div>
   );
 }
---
+++ mi-tienda/src/components/NavBar.js
@@ -0,0 +1,24 @@
+const Navbar = () => { 
+    return (
+<nav class = "navbar navbar-expand  navbar-dark bg-dark">
+        <div class = "container-fluid flex-row-reverse">
+            <ul class= "navbar-nav">
+                <li class="nav-item m-2">
+                    <a class="nav-link" href="#">P1</a>
+                </li>
+                <li class="nav-item m-2">
+                    <a class="nav-link" href="#">P2</a>
+                </li>
+                <li class="nav-item m-2">
+                    <a class="nav-link" href="#">P3</a>
+                </li>
+                <li class="nav-item m-2">
+                    <a class="nav-link" href="#">P4</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    );
+}
+
+export default Navbar;
